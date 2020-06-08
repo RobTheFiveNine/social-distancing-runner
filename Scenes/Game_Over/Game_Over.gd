daa8011fec1dc137ef62ed24051217ca57541a1e
RobTheFiveNine <49003204+RobTheFiveNine@users.noreply.github.com>
2020-06-08 13:30:56 +0100

    Move score calculation into globals script

--- Scenes/Game_Over/Game_Over.gd
+++ Scenes/Game_Over/Game_Over.gd
@@ -7,18 +7,14 @@ func _input(event):
 		get_tree().change_scene("res://Scenes/Title/Title.tscn")
 
 func _ready():
+	Globals.recalculate_score()
 	$CanvasLayer/MarginContainer2/Input_Notice.visible = false
 
-	var final_score = 0
-	final_score += (Globals.covidiots_defeated * 10) * (Globals.covidiots_defeated / Globals.covidiots_generated)
-	final_score += (Globals.items_collected * 5) * (Globals.items_collected / Globals.items_available)
-	final_score += Globals.time_remaining + Globals.coin_value_collected
-	
 	set_stat("Items_Collected", Globals.items_collected, Globals.items_available)
 	set_stat("Covidiots_Defeated", Globals.covidiots_defeated, Globals.covidiots_generated)
 	set_stat("Time_Bonus", Globals.time_remaining)
 	set_stat("Coin_Bonus", Globals.coin_value_collected)
-	set_stat("MarginContainer2/Final_Score", final_score)
+	set_stat("MarginContainer2/Final_Score", Globals.score)
 	
 	if Globals.died:
 		$CanvasLayer/MarginContainer3/Game_Over_Title.text = "GAME OVER"
@@ -38,6 +34,6 @@ func set_stat(name, value, total = null):
 			"%d" % value
 		)
 
-func _on_Transition_unblanked(tag):
+func _on_Transition_unblanked(_tag):
 	listen_for_input = true
 	$CanvasLayer/MarginContainer2/Input_Notice.visible = true
--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -10,3 +10,9 @@ export (int) var time_remaining = 0
 export (int) var coin_value_collected = 0
 
 export (bool) var died = false
+export (int) var score = 0
+
+func recalculate_score():
+	score = (Globals.covidiots_defeated * 10) * (Globals.covidiots_defeated / Globals.covidiots_generated)
+	score += (Globals.items_collected * 5) * (Globals.items_collected / Globals.items_available)
+	score += Globals.time_remaining + Globals.coin_value_collected
